0db2c367c09ebad92314e2814e351ba284615069
IvanPostu <ipostu20000127@gmail.com>
2024-05-06 21:19:25 +0300

    004 - Windows Platform Layer

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,5 +1,7 @@
 #include <core/logger.h>
 #include <core/asserts.h>
+// TODO: Test
+#include <platform/platform.h>
 
 int main(void) {
     KFATAL("A test message: %f", 3.14f);
@@ -8,6 +10,14 @@ int main(void) {
     KINFO("A test message: %f", 3.14f);
     KDEBUG("A test message: %f", 3.14f);
     KTRACE("A test message: %f", 3.14f);
+
+    platform_state state;
+    if (platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1280, 720)) {
+        while (TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     KASSERT(1 == 0);
 
