ace0e88b0fef6f653d703b3d143a4f31514538ca
IvanPostu <ipostu20000127@gmail.com>
2024-05-11 14:33:57 +0300

    022 - Build system and math lib part 1

--- engine/src/defines.h
+++ engine/src/defines.h
@@ -103,3 +103,11 @@ STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 
 #define KCLAMP(value, min, max) (value <= min) ? min : (value >= max) ? max \
                                                                       : value;
+// Inlining
+#ifdef _MSC_VER
+#define KINLINE __forceinline
+#define KNOINLINE __declspec(noinline)
+#else
+#define KINLINE static __attribute__((always_inline))
+#define KNOINLINE
+#endif
